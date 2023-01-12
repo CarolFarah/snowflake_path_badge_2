2a90aa9206182164e585233acc94140afdac76a6
Ana Carolina Farah <anacarolfarah@gmail.com>
2023-01-12 15:45:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueerry Oatmeal')
+streamlit.text('Kale, Spinach &  Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
