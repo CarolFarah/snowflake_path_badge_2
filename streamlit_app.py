66dc1abd445a320b7567d31dc020ddf782964a81
Ana Carolina Farah <anacarolfarah@gmail.com>
2023-01-12 16:25:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,6 @@ streamlit.dataframe(my_fruit_list)
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-ststreamlit.multselect("Pick some fruits:", list(my_fruit_list.index))
+ststreamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
