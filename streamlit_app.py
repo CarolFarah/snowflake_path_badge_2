204ccbaf465b288b57d31d80ab733612c79f63ae
Ana Carolina Farah <anacarolfarah@gmail.com>
2023-01-31 17:25:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 # Display the table on the page.
 
 streamlit.header('Fruitvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?' + "")
 streamlit.write('The user entered ', fruit_choice)
 
 fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
